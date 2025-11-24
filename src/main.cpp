ac8ed3fa9cfed5676ad9c9029e7eb8aabc9e8abf
Sunset-06 <nilay.sabnis05@gmail.com>
2025-11-24 18:08:32 +0530

    added a shader

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@ const int SCREEN_HEIGHT = 480;
 
 bool exit_flag = false;
 
+float triangle[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+unsigned int VBO;
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
 
 void setup(){
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -66,6 +80,23 @@ int main(){
     std::cout << "Glad Initialized\n";
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    else{
+        std::cout << "Shaader compiled\n";
+    }
+
+
     SDL_Event e;
 
     while (!exit_flag) {
@@ -74,6 +105,14 @@ int main(){
                 exit_flag = true;
             }
         }
+        glClearColor(0.0f, 0.35f, 0.4f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // creating buffer vbo and assigning id 1
+        glGenBuffers(1, &VBO);
+        // binding vbo as an array buffer
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);    
+        SDL_GL_SwapWindow(window);
     }
 
     SDL_DestroyWindow(window);
